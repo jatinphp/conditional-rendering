52b1a2de8a4932fd608bc7c974b12158d04a9a60
Jatin <32764697+jatinphp@users.noreply.github.com>
2024-07-26 07:18:27 +0000

    after pratice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Login from "./Login";
+import Form from "./Form";
+
+var isLoggedIn = false;
+const currentTime = new Date().getHours();
+var isRegisteredUser = false;
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {isRegisteredUser ? <Login /> : <Form />}
+      {currentTime > 12 ? <h1>Why are you Still working?</h1> : null}
     </div>
   );
 }
