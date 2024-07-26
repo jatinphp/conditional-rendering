710cd2f331ce2c39f31ada9325fcd3b6a069ebe3
Jatin <32764697+jatinphp@users.noreply.github.com>
2024-07-26 13:12:52 +0000

    useState from react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./Login";
 import Form from "./Form";
 
@@ -7,9 +7,20 @@ const currentTime = new Date().getHours();
 var isRegisteredUser = false;
 
 function App() {
+  const [count, setCount] = useState(0);
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
   return (
     <div className="container">
-      {isRegisteredUser ? <Login /> : <Form />}
+      <h1>{count}</h1>
+      <button onClick={increase}>+++</button>
+      <button onClick={decrease}>---</button>
+      <br />
+      <Form isRegistered={isRegisteredUser} />
       {currentTime > 12 ? <h1>Why are you Still working?</h1> : null}
     </div>
   );
